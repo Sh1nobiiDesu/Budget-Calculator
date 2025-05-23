c5ff0d5e0a5925f2976375373ff6e093f6d52927
MarkTangon <marktangon111803@gmail.com>
2025-05-22 23:44:22 -0600

    Started Project

---
+++ EasyExcel/EasyExcel/Table.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace EasyExcel
+{
+    partial class Table
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.BTN_Calculate = new System.Windows.Forms.Button();
+            this.BTN_Save = new System.Windows.Forms.Button();
+            this.BTN_Delete = new System.Windows.Forms.Button();
+            this.BTN_Edit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(737, 416);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // BTN_Calculate
+            // 
+            this.BTN_Calculate.Location = new System.Drawing.Point(647, 444);
+            this.BTN_Calculate.Name = "BTN_Calculate";
+            this.BTN_Calculate.Size = new System.Drawing.Size(102, 23);
+            this.BTN_Calculate.TabIndex = 1;
+            this.BTN_Calculate.Text = "Calculate";
+            this.BTN_Calculate.UseVisualStyleBackColor = true;
+            // 
+            // BTN_Save
+            // 
+            this.BTN_Save.Location = new System.Drawing.Point(539, 444);
+            this.BTN_Save.Name = "BTN_Save";
+            this.BTN_Save.Size = new System.Drawing.Size(102, 23);
+            this.BTN_Save.TabIndex = 2;
+            this.BTN_Save.Text = "Save";
+            this.BTN_Save.UseVisualStyleBackColor = true;
+            // 
+            // BTN_Delete
+            // 
+            this.BTN_Delete.Location = new System.Drawing.Point(12, 444);
+            this.BTN_Delete.Name = "BTN_Delete";
+            this.BTN_Delete.Size = new System.Drawing.Size(102, 23);
+            this.BTN_Delete.TabIndex = 3;
+            this.BTN_Delete.Text = "Delete";
+            this.BTN_Delete.UseVisualStyleBackColor = true;
+            // 
+            // BTN_Edit
+            // 
+            this.BTN_Edit.Location = new System.Drawing.Point(120, 444);
+            this.BTN_Edit.Name = "BTN_Edit";
+            this.BTN_Edit.Size = new System.Drawing.Size(102, 23);
+            this.BTN_Edit.TabIndex = 4;
+            this.BTN_Edit.Text = "Edit";
+            this.BTN_Edit.UseVisualStyleBackColor = true;
+            // 
+            // Table
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 479);
+            this.Controls.Add(this.BTN_Edit);
+            this.Controls.Add(this.BTN_Delete);
+            this.Controls.Add(this.BTN_Save);
+            this.Controls.Add(this.BTN_Calculate);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Table";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button BTN_Calculate;
+        private System.Windows.Forms.Button BTN_Save;
+        private System.Windows.Forms.Button BTN_Delete;
+        private System.Windows.Forms.Button BTN_Edit;
+    }
+}
+
---
+++ EasyExcel/UserAdd/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserAdd
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            TXTB_Units.TextChanged += TXTB_Units_TextChanged;
+        }
+
+        private void TXTB_Units_TextChanged(object sender, EventArgs e)
+        {
+            string text = "Price per ";
+            LBL_Price.Text = text + TXTB_Units.Text;
+
+        }
+    }
+}
